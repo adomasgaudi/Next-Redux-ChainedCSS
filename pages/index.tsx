198d6bca7ad52c1c17ee55077b3f1df1c34a8eae
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2022-10-24 01:18:57 +0300

    feat: add chunkyui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,14 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
-
-import { Header, Main, Cards, Footer } from "@components/scss";
+import { _ } from "chainedcss";
+import { OContainer } from "chunkyui/components/Container";
 
 const Home: React.FC = () => {
     return (
-        <div
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                minHeight: "100vh",
-            }}
-        >
-            <Header />
-            <Main />
-            <Cards />
-            <Footer />
-        </div>
+        <OContainer.base>
+            <h1 {..._.textCenter()}>oh hi mark</h1>
+            <p>oh hi mark</p>
+        </OContainer.base>
     );
 };
 
---
+++ src/components/othercomps/Header.styles.tsx
@@ -0,0 +1,19 @@
+// import React from "react";
+// import Image from "next/image";
+// import { _ } from "../../../ossom";
+
+// const img = "https://miro.medium.com/max/880/0*t9RJ9HwzlN8Miylg.png";
+
+// export const Logo = () => {
+//   return (
+//     <div {..._.w(48).h(48).relative()}>
+//       {/* <Image
+//         src={img}
+//         // layout='fill'
+//         alt='cube'
+//         // {..._.bg`$f4dbd7`.roundedLg().p3().absolute()}
+//       /> */}
+//     </div>
+//   );
+// };
+export {};
